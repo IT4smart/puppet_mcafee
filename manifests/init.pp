a0041d90602d4d3b5d2122e1a4b1687760351ae9
hack3d <hack3d.dev@googlemail.com>
2024-02-05 20:32:23 +0000

    Add comma

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,7 @@ class puppet_mcafee {
 
   file { 'C:\\Windows\\Temp\\remove_mcafee.ps1':
     ensure  => present,
-    content => @(EOF)
+    content => @(EOF),
       # Run the cleanup tool
       $program= "c:\Windows\Temp\McCleanup.exe"
       $programArg= "-p StopServices,MFSY,PEF,MXD,CSP,Sustainability,MOCP,MFP,APPSTATS,Auth,EMproxy,FWdiver,HW,MAS,MAT,MBK,MCPR,McProxy,McSvcHost,VUL,MHN,MNA,MOBK,MPFP,MPFPCU,MPS,SHRED,MPSCU,MQC,MQCCU,MSAD,MSHR,MSK,MSKCU,MWL,NMC,RedirSvc,VS,REMEDIATION,MSC,YAP,TRUEKEY,LAM,PCB,Symlink,SafeConnect,MGS,WMIRemover,RESIDUE -v -s"
