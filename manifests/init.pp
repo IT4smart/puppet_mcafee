72fd6fe94b19b6ebd7d113d504aa555e12ff89b5
hack3d <hack3d.dev@googlemail.com>
2024-02-05 20:30:55 +0000

    Move powershell script to inline

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,8 +8,19 @@ class puppet_mcafee {
   } ->
 
   file { 'C:\\Windows\\Temp\\remove_mcafee.ps1':
-    ensure => present,
-    source => 'puppet:///modules/puppet_mcafee/remove_mcafee.ps1',
+    ensure  => present,
+    content => @(EOF)
+      # Run the cleanup tool
+      $program= "c:\Windows\Temp\McCleanup.exe"
+      $programArg= "-p StopServices,MFSY,PEF,MXD,CSP,Sustainability,MOCP,MFP,APPSTATS,Auth,EMproxy,FWdiver,HW,MAS,MAT,MBK,MCPR,McProxy,McSvcHost,VUL,MHN,MNA,MOBK,MPFP,MPFPCU,MPS,SHRED,MPSCU,MQC,MQCCU,MSAD,MSHR,MSK,MSKCU,MWL,NMC,RedirSvc,VS,REMEDIATION,MSC,YAP,TRUEKEY,LAM,PCB,Symlink,SafeConnect,MGS,WMIRemover,RESIDUE -v -s"
+      $process = Start-Process $program -ArgumentList $ProgramArg -passthru -Wait -NoNewWindow
+
+      # Remove the Store apps from McAfee
+      $RemoveApp = 'Mcafee'
+      Get-AppxPackage -AllUsers | Where-Object {$_.Name -Match $RemoveApp} | Remove-AppxPackage
+      Get-AppxPackage | Where-Object {$_.Name -Match $RemoveApp} | Remove-AppxPackage
+      Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -Match $RemoveApp} | Remove-AppxProvisionedPackage -Online
+    EOF
   } ->
 
   exec { 'uninstall-mcafee':
